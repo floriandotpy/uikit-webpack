48faa8071d5102e8c0810950b63c12dde14d1992
Florian <florian@yootheme.com>
2017-03-01 14:37:19 +0100

    uikit update, import from dist

--- app/index.js
+++ app/index.js
@@ -1,15 +1,11 @@
 // jQuery and UIkit core
-import UIkit from 'uikit/src/js/uikit';
+import UIkit from 'uikit';
 import $ from 'jquery';
 
-// Additional UIkit components (more available then these two)
-import Notification from 'uikit/src/js/components/notification'
-import Tooltip from 'uikit/src/js/components/tooltip';
+console.log(UIkit);
 
+window.UIkit = UIkit;
 
-// Additional components need to be registered and initialized with UIkit
-UIkit.use(Tooltip);
-UIkit.use(Notification);
 
 // Some components will hook onto existing markup (e.g. tooltip), others can be called:
 UIkit.notification({
